14946404de522f4e60392e88b81f940539386f2f
Dmitry Laptev <dvlapte3@mts.ru>
2021-04-07 23:43:42 +0300

    Major improvements

--- customls.c
+++ customls.c
@@ -6,64 +6,94 @@
 #include <pwd.h>
 #include <grp.h>
 #include <time.h> 
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 
-int printRow(char *filePath, char* fileName){
+#define PATH_BUF_LENGTH 4096
 
-  char timeBuff[20]; // Buffer for time string
-  char linkBuff[128];
-  struct passwd *userData; // Struct for getting data from userID
-  struct group *groupData; // Struct for getting data from groupID
-  struct stat fileStat;
-  if(lstat(filePath,&fileStat) < 0)    
+int print_row(char *file_path, char* file_name){
+
+  char time_buff[20]; // Buffer for time string
+  char link_buff[PATH_BUF_LENGTH];
+  struct passwd user_data; // Struct for getting data from userID
+  struct group group_data; // Struct for getting data from groupID
+  struct stat file_stat;
+
+  if(lstat(file_path,&file_stat) < 0)    
       return 1;
 
-  userData = getpwuid(fileStat.st_uid);
-  groupData = getgrgid(fileStat.st_uid);
+  user_data = *getpwuid(file_stat.st_uid);
+  group_data = *getgrgid(file_stat.st_uid);
 
-  strftime(timeBuff, 20, "%b %e %H:%M", localtime(&fileStat.st_mtime));
+  strftime(time_buff, 20, "%b %e %H:%M", localtime(&file_stat.st_mtime));
 
   // Permissions:
-  putchar( (S_ISDIR(fileStat.st_mode)) ? 'd' : '-');
-  putchar( (fileStat.st_mode & S_IRUSR) ? 'r' : '-');
-  putchar( (fileStat.st_mode & S_IWUSR) ? 'w' : '-');
-  putchar( (fileStat.st_mode & S_IXUSR) ? 'x' : '-');
-  putchar( (fileStat.st_mode & S_IRGRP) ? 'r' : '-');
-  putchar( (fileStat.st_mode & S_IWGRP) ? 'w' : '-');
-  putchar( (fileStat.st_mode & S_IXGRP) ? 'x' : '-');
-  putchar( (fileStat.st_mode & S_IROTH) ? 'r' : '-');
-  putchar( (fileStat.st_mode & S_IWOTH) ? 'w' : '-');
-  putchar( (fileStat.st_mode & S_IXOTH) ? 'x' : '-');
+  putchar( (S_ISDIR(file_stat.st_mode)) ? 'd' : '-');
+  putchar( (file_stat.st_mode & S_IRUSR) ? 'r' : '-');
+  putchar( (file_stat.st_mode & S_IWUSR) ? 'w' : '-');
+  putchar( (file_stat.st_mode & S_IXUSR) ? 'x' : '-');
+  putchar( (file_stat.st_mode & S_IRGRP) ? 'r' : '-');
+  putchar( (file_stat.st_mode & S_IWGRP) ? 'w' : '-');
+  putchar( (file_stat.st_mode & S_IXGRP) ? 'x' : '-');
+  putchar( (file_stat.st_mode & S_IROTH) ? 'r' : '-');
+  putchar( (file_stat.st_mode & S_IWOTH) ? 'w' : '-');
+  putchar( (file_stat.st_mode & S_IXOTH) ? 'x' : '-');
   putchar(' ');
-  printf("%lu",fileStat.st_nlink); // Num of links
+  printf("%lu",file_stat.st_nlink); // Num of links
   putchar(' ');
-  printf("%s", userData->pw_name); // Owner's username
+  printf("%s", user_data.pw_name); // Owner's username
   putchar(' ');
-  printf("%s", groupData->gr_name); // Owner's groupname
+  printf("%s", group_data.gr_name); // Owner's groupname
   putchar(' ');
-  printf("%ld", fileStat.st_size); // File size
+  printf("%ld", file_stat.st_size); // File size
   putchar(' ');
-  printf("%s", timeBuff);
+  printf("%s", time_buff);
   putchar(' ');
-  if(S_ISLNK(fileStat.st_mode)) {
-    readlink(filePath, (char*)&linkBuff, 128);
-    printf("%s -> ", fileName);
-    printf("%s", linkBuff);
+
+  if(S_ISLNK(file_stat.st_mode)) { // If dir is symbolic link
+    readlink(file_path, (char*)&link_buff, PATH_BUF_LENGTH);
+    printf("%s -> ", file_name);
+    printf("%s", link_buff);
     putchar('\b');
   }
   else
-    printf("%s", fileName);
+    printf("%s", file_name);
+
   putchar('\n');
-  //free(userData);
-  //free(groupData);
   return 0;
+}
+
+void open_dir(char* dirname) {
+  DIR *d;
+  char *path_buf;
+  struct dirent *dir;
+  int errnum;
+
+  d = opendir(dirname);
+
+  if (d) { // Open dir success
+
+    while ((dir = readdir(d)) != NULL) {
+
+      path_buf = malloc(sizeof(dirname) + sizeof(dir->d_name) + 2);
+      sprintf(path_buf, "%s/%s", dirname, dir->d_name);
+      print_row(path_buf, dir->d_name);
+    }
+
+    closedir(d);
+    free(path_buf);
+  }
+  else {
+    errnum = errno;
+    fprintf(stderr, "ls: %s: %s\n", dirname, strerror(errnum));
+  }
 }
 
 int main(int argc, char **argv)
 {
   int i;
-  int numMinL=0; // Number of -l in arg list
-  int pathsCount=0; // Quantity of paths
-  char *paths[32]; // Arr of paths
+  int num_min_l=0; // Number of -l in arg list
 
   // If no arguments
   if(argc < 2) {
@@ -71,41 +101,25 @@ int main(int argc, char **argv)
     return 1;
   }   
   // Search for '-l' argument
-  for(i=1;i<argc;i++){
-    if((argv[i][0]=='-')&&(argv[i][1]=='l')){
-      numMinL=i;
+  for(i = 1;i < argc;i++) {
+    if(strcmp(argv[i], "-l") == 0){
+      num_min_l = i;
       break;
     }
   }
   // If no '-l' argument
-  if(numMinL==0) { 
+  if(num_min_l == 0) { 
     printf("Please use -l argument\n");
     return 1;
   }
-  // Saving all paths
-  for(i=1;i<argc;i++){
-    if(i!=numMinL)
-      paths[pathsCount++]=argv[i];
+
+  for(i = 1;i < argc;i++){ // Opening dirs by paths
+
+    if(i == num_min_l) continue; // Skipping -l as path
+
+    open_dir(argv[i]);
   }
 
-  DIR *d;
-  char pathBuf[258];
-  struct dirent *dir;
-
-  for(i=0;i<pathsCount;i++){
-    d = opendir(paths[i]);
-    if (d){
-      readdir(d);
-      readdir(d); // Crutch to skip this and parent dir
-      while ((dir = readdir(d)) != NULL) {
-        sprintf(pathBuf, "%s/%s", paths[i], dir->d_name);
-        printRow(pathBuf, dir->d_name);
-      }
-      closedir(d);
-    }
-    else {
-      printf("ls: cannot access '%s': No such file or directory\n", paths[i]);
-    }
-  }
+  if(i == 2) open_dir("."); // When no arguments excepts -l
 }
 
